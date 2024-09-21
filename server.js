fd7d20e0f082ebddb0667bd278c8b1bb312a43ca
nelsonFiho22222 <nelsoncamarafilho@gmail.com>
2024-09-21 17:01:22 -0300

    put delete

--- server.js
+++ server.js
@@ -1,20 +1,68 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const users = []
 
+app.post('/usuarios', async (req, res) => {
 
-app.post('/usuarios', (req, res) => {
-
-    users.push(req.body)
-
-    res.send('Deu tudo certo')
+    await prisma.users.create({
+        data: {
+            email: req.body.email,
+            name : req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res) => {
-    res.json(users)
+app.get('/usuarios', async (req, res) => {
+    let users = [];
+
+    try {
+        if(req.query.name){
+            users = await prisma.users.findMany({
+                where: {
+                    name: req.query.name
+                }
+            });
+        } else {
+            users = await prisma.users.findMany();
+        }
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Erro ao buscar usuários: ", error);
+        res.status(500).json({ message: "Erro ao buscar usuários." });
+    }
+});
+
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.   users.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name : req.body.name,
+            age: req.body.age
+        }
+    });
+    res.status(200).json(req.body)
+});
+
+app.delete("usuarios/:id", async (req, res) => {
+    await prisma.users.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.status(200).json({message : ' Usuario deletado com Sucesso'})
 })
 
 app.listen(3000);
